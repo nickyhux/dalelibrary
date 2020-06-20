21bb00b60a4fc67abbf496f7c7c6cd3d1d9375fd
nickyhux <nicolehuxta@gmail.com>
2020-06-20 15:24:41 -0400

    please work lol

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'wwyme%2!*85n+ca7#(w9l5a7_rl!%f
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG','') != 'False'
 
-ALLOWED_HOSTS = ['https://murmuring-wildwood-74383.herokuapp.com/', 'http://127.0.0.1/']
+ALLOWED_HOSTS = ['murmuring-wildwood-74383.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
